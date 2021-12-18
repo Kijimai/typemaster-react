c7d0a476bc11c82cde8665aabffc1504bbfabfb4
Jibbyworks <JDBucog@yahoo.com>
2021-12-18 03:18:40 -0800

    nav mobile

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,9 +2,10 @@ import React from "react"
 import Button from "./Button"
 import heroKeyboardDesktop from "../images/desktop/image-keyboard.jpg"
 import heroKeyboardMobile from "../images/mobile/image-keyboard.jpg"
+import { useGlobalContext } from "../utils/context"
 
 const Hero = () => {
-  
+  const { isMobile, viewWidth } = useGlobalContext()
 
   return (
     <div className="hero flex">
@@ -14,12 +15,15 @@ const Hero = () => {
           Improve your productivity and gaming without breaking the bank.
           Upgrade to a high quality mechanical typing experience.
         </p>
-        <Button btnModifier={"btn-2"} btnTitle={"Pre-order Now"} />
+        <div className="pre-order flex">
+          <Button btnModifier={"btn-2"} btnTitle={"Pre-order Now"} />
+          <p className="pre-order__text">Release on 5/27</p>
+        </div>
       </div>
       <div className="hero__image-container">
         <img
           className="hero__image"
-          src={heroKeyboardMobile}
+          src={viewWidth < 900 ? heroKeyboardMobile : heroKeyboardDesktop}
           alt="featured preview of keyboard"
         />
       </div>
